67470ca2bebeca78237925e0736e595eb8700259
matchi01 <mateusz.w.twardy@gmail.com>
2021-09-05 15:41:29 +0200

    Changed branch, Added proptypes and useReact libraries

--- src/components/ListVisits.js
+++ src/components/ListVisits.js
@@ -5,71 +5,116 @@ import Box from "@material-ui/core/Box";
 import ProgressCircle from "./ProgressCircle";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import PropTypes from 'prop-types';
+import {useAsyncFn} from 'react-use';
 
-const ListVisits = ({ handleVisit }) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [availableVisits, setAvailableVisits] = useState([]);
-  const [isError, setIsError] = useState(false);
 
-  const fetchData = async () => {
-    setIsLoading(true);
-    setIsError(false);
-    try {
-      const response = await fetch("data/data.json");
-      const data = await response.json();
-      setAvailableVisits(data);
-    } catch (error) {
-      setIsError(true);
-      console.log(error);
-    }
-    setIsLoading(false);
-  };
+const ListVisits = ({handleVisit}) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [availableVisits, setAvailableVisits] = useState([]);
+    const [isError, setIsError] = useState(false);
+    const url = "data/data.json";
 
-  useEffect(() => {
-    fetchData();
-  }, []);
 
-  const [currentIndex, setCurrentIndex] = useState();
+    // const Demo = ({url}) => {
+    //     const [state, doFetch] = useAsyncFn(async () => {
+    //         const response = await fetch(url);
+    //         const result = await response.json();
+    //         return result
+    //     }, [url])
+    // };
 
-  const chooseCurrentVisit = (visit) => {
-    handleVisit(visit);
-    setCurrentIndex(visit.id);
-  };
 
-  return (
-    <Grid container item md={6} >
-      {isError ? (
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          margin="0 auto"
-          flexDirection="column"
-        >
-          <Typography> Problem z ładowaniem danych</Typography>
-          <Button
-            variant="contained"
-            color="secondary"
-            disableElevation
-            onClick={fetchData}
-          >
-            Spróbuj ponownie
-          </Button>
-        </Box>
-      ) : isLoading ? (
-        <ProgressCircle />
-      ) : (
-        availableVisits.map((visit) => (
-          <VisitCard
-            key={visit.id}
-            visit={visit}
-            active={visit.id === currentIndex}
-            chooseCurrentVisit={() => chooseCurrentVisit(visit)}
-          />
-        ))
-      )}
-    </Grid>
-  );
+    const fetchData = async () => {
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        const response = await fetch("data/data.json");
+        const data = await response.json();
+        setAvailableVisits(data);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+    const [currentIndex, setCurrentIndex] = useState();
+
+    const chooseCurrentVisit = (visit) => {
+        handleVisit(visit);
+        setCurrentIndex(visit.id);
+    };
+
+    return (
+        <Grid container item md={6}>
+          {/*  {state.loading ?*/}
+          {/*      <ProgressCircle/>*/}
+          {/*      : state.error*/}
+          {/*          ? <Box*/}
+          {/*              display="flex"*/}
+          {/*              alignItems="center"*/}
+          {/*              justifyContent="center"*/}
+          {/*              margin="0 auto"*/}
+          {/*              flexDirection="column"*/}
+          {/*          >*/}
+          {/*              <Typography> {state.error.message}</Typography>*/}
+          {/*          </Box> :*/}
+          {/*          availableVisits.map((visit) => (*/}
+          {/*              <VisitCard*/}
+          {/*                  key={visit.id}*/}
+          {/*                  visit={visit}*/}
+          {/*                  active={visit.id === currentIndex}*/}
+          {/*                  chooseCurrentVisit={() => chooseCurrentVisit(visit)}*/}
+          {/*              />*/}
+          {/*          ))*/}
+          {/*  }*/}
+          {/*<button onClick={() => doFetch()}>Start loading</button>*/}
+
+
+            {isError ? (
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                margin="0 auto"
+                flexDirection="column"
+              >
+                <Typography> Problem z ładowaniem danych</Typography>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  disableElevation
+                  onClick={fetchData}
+                >
+                  Spróbuj ponownie
+                </Button>
+              </Box>
+            ) : isLoading ? (
+              <ProgressCircle />
+            ) : (
+              availableVisits.map((visit) => (
+                <VisitCard
+                  key={visit.id}
+                  visit={visit}
+                  active={visit.id === currentIndex}
+                  chooseCurrentVisit={() => chooseCurrentVisit(visit)}
+                />
+              ))
+            )}
+        </Grid>
+    );
 };
 
+ListVisits.propTypes = {
+    handleVisit: PropTypes.func.isRequired
+}
+
+
 export default ListVisits;
+
+
